3716176a97ba90de554030b88d9cf65742430280
Ujaan Das <ujaandas03@gmail.com>
2025-05-13 11:19:37 +0800

    try tty

--- flake.nix
+++ flake.nix
@@ -15,7 +15,39 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (final: prev: {
+              pycodec2 = prev.python3Packages.buildPythonPackage {
+                pname = "pycodec2";
+                version = "4.1.0";
+
+                src = prev.fetchPypi {
+                  pname = "pycodec2";
+                  version = "4.1.0";
+                  sha256 = "46a491f4c8e2328cb633b40ef6dccbd2ea08da51f6b76e795c4d7a439f8d355b";
+                };
+
+                buildInputs = with pkgs; [
+                  codec2
+                ];
+
+                propagatedBuildInputs = with prev.python3Packages; [
+                  cython
+                  numpy
+                ];
+
+                meta = with prev.lib; {
+                  description = "Python binding for codec2";
+                  license = prev.lib.licenses.mit;
+                  platforms = prev.lib.platforms.all;
+                };
+
+              };
+            })
+          ];
+        };
       in
       {
         packages = rec {
@@ -24,11 +56,19 @@
             version = "0.1.0";
             src = ./app;
 
-            nativeBuildInputs = with pkgs; [ portaudio ];
+            nativeBuildInputs = with pkgs; [
+              codec2
+              portaudio
+              sox
+            ];
 
             buildInputs = with pkgs; [ cowsay ];
 
-            propagatedBuildInputs = with pkgs.python3Packages; [ pyaudio ];
+            propagatedBuildInputs = with pkgs; [
+              pycodec2
+              python3Packages.pyaudio
+              python3Packages.soxr
+            ];
 
             meta = with pkgs.lib; {
               description = "Volora";
@@ -47,9 +87,8 @@
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             python3
-            portaudio
             cowsay
-            python3Packages.pyaudio
+            python3Packages.pip
           ];
         };
       }
