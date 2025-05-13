8a09070518ddf2cef50c3b9ce7e65c0ee0b113b4
Ujaan Das <ujaandas03@gmail.com>
2025-05-13 10:35:26 +0800

    finally fixed nix and python deps

--- flake.nix
+++ flake.nix
@@ -1,102 +1,56 @@
 {
-  description = "Hello world flake using uv2nix";
+  description = "peepee poopoo";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    pyproject-nix = {
-      url = "github:pyproject-nix/pyproject.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    uv2nix = {
-      url = "github:pyproject-nix/uv2nix";
-      inputs.pyproject-nix.follows = "pyproject-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    pyproject-build-systems = {
-      url = "github:pyproject-nix/build-system-pkgs";
-      inputs.pyproject-nix.follows = "pyproject-nix";
-      inputs.uv2nix.follows = "uv2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      uv2nix,
-      pyproject-nix,
-      pyproject-build-systems,
       flake-utils,
-      ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        inherit (nixpkgs) lib;
-        workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
-
-        overlay = workspace.mkPyprojectOverlay {
-          sourcePreference = "wheel";
-        };
-
-        pkgs = nixpkgs.legacyPackages.${system};
-        python = pkgs.python312;
-
-        pythonSet =
-          (pkgs.callPackage pyproject-nix.build.packages {
-            inherit python;
-          }).overrideScope
-            (
-              lib.composeManyExtensions [
-                pyproject-build-systems.overlays.default
-                overlay
-              ]
-            );
-
-        interface = pythonSet.mkVirtualEnv "interface-env" workspace.deps.default;
+        pkgs = import nixpkgs { inherit system; };
       in
-      rec {
-        packages = {
-          default = interface;
-        };
-        defaultPackage = interface;
+      {
+        packages = rec {
+          volora = pkgs.python3Packages.buildPythonPackage {
+            pname = "volora";
+            version = "0.1.0";
+            src = ./app;
 
-        apps = {
-          default = {
-            type = "app";
-            program = "${interface}/bin/interface";
+            nativeBuildInputs = [ pkgs.portaudio ];
+
+            buildInputs = [ pkgs.cowsay ];
+
+            propagatedBuildInputs = [ pkgs.python3Packages.pyaudio ];
+
+            meta = with pkgs.lib; {
+              description = "A basic Python package that prints greeting via cowsay, with PyAudio and PortAudio dependencies";
+              license = licenses.mit;
+              platforms = platforms.all;
+            };
+
+            postFixup = ''
+              wrapProgram $out/bin/volora --prefix PATH : ${pkgs.cowsay}/bin
+            '';
           };
+
+          default = volora;
         };
-        defaultApp = apps.default;
 
         devShell = pkgs.mkShell {
-          buildInputs = [
-            python
-            pkgs.uv
-            pkgs.portaudio
-            pkgs.codec2
-            pkgs.sox
-            pkgs.cowsay
+          buildInputs = with pkgs; [
+            python3
+            portaudio
+            cowsay
+            python3Packages.pyaudio
           ];
-          env =
-            {
-              UV_PYTHON_DOWNLOADS = "never";
-              UV_PYTHON = python.interpreter;
-            }
-            // lib.optionalAttrs pkgs.stdenv.isLinux {
-              LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
-            };
-          shellHook = ''
-            unset PYTHONPATH
-          '';
         };
       }
     );
